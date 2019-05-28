d6cb2a423bb2eeb728895d5b7f09bc3cf52f2b16
Vinh Nguyen <nguyenv2@outlook.com>
2019-05-28 10:10:13 -0700

    flip

--- basics/Main.java
+++ basics/Main.java
@@ -4,6 +4,8 @@ public class Main {
     int catCount = 1;
     System.out.println("I own " + dogCount + " " + pluralize(dogCount, "dog") + " and " + catCount + " " + pluralize(catCount, "cat"));
 
+    flipNHeads(2);
+
   }
 
   public static String pluralize(int numberInput, String stringInput) {
@@ -12,5 +14,22 @@ public class Main {
     } else {
       return stringInput + "s";
     }
+  }
+
+  public static void flipNHeads(int nFLips) {
+    int overallCount = 0;
+    int headCount = 0;
+    while(headCount < nFLips) {
+      double flipResult = Math.random();
+      if(flipResult < .5) {
+        System.out.println("tails");
+        headCount = 0;
+      } else {
+        System.out.println("heads");
+        headCount++;
+      }
+      overallCount++;
+    }
+    System.out.println("It took " + overallCount + " flips to flip " + nFLips + " in a row.");
   }
 }
