04d22357ae93b415fc4990d87651610278400b53
Vinh Nguyen <nguyenv2@outlook.com>
2019-05-28 10:42:12 -0700

    clock

--- basics/Main.java
+++ basics/Main.java
@@ -1,11 +1,14 @@
+import java.time.LocalDateTime;
+import java.time.Duration;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
   public static void main(String[] args) {
     int dogCount = 2;
     int catCount = 1;
     System.out.println("I own " + dogCount + " " + pluralize(dogCount, "dog") + " and " + catCount + " " + pluralize(catCount, "cat"));
-
     flipNHeads(2);
-
+    clock();
   }
 
   public static String pluralize(int numberInput, String stringInput) {
@@ -31,5 +34,19 @@ public class Main {
       overallCount++;
     }
     System.out.println("It took " + overallCount + " flips to flip " + nFLips + " in a row.");
+  }
+
+  public static void clock() {
+    LocalDateTime oldTime = LocalDateTime.now();
+    while(true) {
+      LocalDateTime newTime = LocalDateTime.now();
+      Duration timeDiff = Duration.between(oldTime, newTime);
+      long seconds = timeDiff.getSeconds();
+      if(seconds >= 1) {
+        String time = newTime.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        System.out.println(time);
+        oldTime = newTime;
+      }
+    }
   }
 }
